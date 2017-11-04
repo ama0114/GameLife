cc93c602844390efe44cd368a49c29d224b526e0
Antonio de los Mozos Alonso <ama0114@alu.ubu.es>
2017-11-04 01:56:58 +0100

    Some translations

--- src/GameLife.java
+++ src/GameLife.java
@@ -9,79 +9,80 @@ public class GameLife{
 	static final int tamX = 40;
 	static final int tamY = 150;
 	
-	public static int[][] tablero = new int[tamX][tamY];
+	public static int[][] board = new int[tamX][tamY];
 
 	public static void main(String arg[]) throws InterruptedException {
 		for(int i=0;i<tamX;i++) {
 			for(int j=0;j<tamY;j++) {
-				tablero[i][j]=0;
+				board[i][j]=0;
 			}
 		}
 		
 		int x = tamX/2;
 		int y = tamY/2;
-		tablero[x][y] = 1;
-		tablero[x-1][y-1] = 1;
-		tablero[x][y+1] = 1;
-		tablero[x+1][y] = 1;
-		tablero[x-1][y] = 1;
+		board[x][y] = 1;
+		board[x-1][y-1] = 1;
+		board[x][y+1] = 1;
+		board[x+1][y] = 1;
+		board[x-1][y] = 1;
 		
 		
 		while(true) {
-			ArrayList<int[]> morir = new ArrayList<int[]>();
-			ArrayList<int[]> vivir = new ArrayList<int[]>();
+			ArrayList<int[]> toKill = new ArrayList<int[]>();
+			ArrayList<int[]> toLive = new ArrayList<int[]>();
 			int vivas = 0;
-			pintaTablero(tablero);
+			paintBoard(board);
 			for(int i=0;i<tamX;i++) {
 				for(int j=0;j<tamY;j++) {
-					if(tablero[i][j]==1)
+					if(board[i][j]==1)
 						vivas++;
 					if(i-1 >= 0 && j-1 >= 0 && i+1 < tamX && j+1<tamY) {
-						if(tablero[i][j]==1 && numVivas(tablero,i,j)==2) {
+						if(board[i][j]==1 && numVivas(board,i,j)==2) {
 							int[] v = {i,j};
-							vivir.add(v);
+							toLive.add(v);
 						}		
-						if(tablero[i][j]==1 && numVivas(tablero,i,j)==3){
+						if(board[i][j]==1 && numVivas(board,i,j)==3){
 							int[] v = {i,j};
-							vivir.add(v);
+							toLive.add(v);
 						}
-						if(tablero[i][j]==1 && numVivas(tablero,i,j)>3){
+						if(board[i][j]==1 && numVivas(board,i,j)>3){
 							int[] v = {i,j};
-							morir.add(v);
+							toKill.add(v);
 						}
-						if(tablero[i][j]==1 && numVivas(tablero,i,j)<3){
+						if(board[i][j]==1 && numVivas(board,i,j)<3){
 							int[] v = {i,j};
-							morir.add(v);
+							toKill.add(v);
 						}
-						if(tablero[i][j]==0 && numVivas(tablero,i,j)==3){
+						if(board[i][j]==0 && numVivas(board,i,j)==3){
 							int[] v = {i,j};
-							vivir.add(v);
+							toLive.add(v);
 						}
 						
 					}
 				}
 			 }
 			
-			 System.out.println("Celulas vivas: " + vivas);
+			 System.out.println("Alive cells: " + vivas);
 			
-			 for(int[] i:morir) {
-				 tablero[i[0]][i[1]]=0;
+			 for(int[] i:toKill) {
+				 board[i[0]][i[1]]=0;
 			 }
-			 for(int[] i:vivir) {
-				 tablero[i[0]][i[1]]=1;
+			 for(int[] i:toLive) {
+				 board[i[0]][i[1]]=1;
 			 }
 			 
-			 generaAleatorias(tablero,0);
+			 //Chane the 0 to the number of random cells you want to generate
+			 generateRandom(board,0);
 			 
 			 Thread.sleep(100);
 		}
 	}
 	
 	//Paint the board
-	public static void pintaTablero(int[][] tablero) {
+	public static void paintBoard(int[][] board) {
 		for(int i=0;i<tamX;i++) {
 			for(int j=0;j<tamY;j++) {
-				if(tablero[i][j]==0) {
+				if(board[i][j]==0) {
 					System.out.print(" ");
 				}else {
 					System.out.print("*");
@@ -93,42 +94,42 @@ public class GameLife{
 	}
 	
 	//Calculate the number of live cells around one
-	public static int numVivas(int[][] tablero, int x, int y) {
-		int retorno = 0;
-		if(tablero[x-1][y-1]==1)
-			retorno++;
-		if(tablero[x+1][y+1]==1)
-			retorno++;
-		if(tablero[x-1][y+1]==1)
-			retorno++;
-		if(tablero[x+1][y-1]==1)
-			retorno++;
-		if(tablero[x][y-1]==1)
-			retorno++;
-		if(tablero[x-1][y]==1)
-			retorno++;
-		if(tablero[x][y+1]==1)
-			retorno++;
-		if(tablero[x+1][y]==1)
-			retorno++;
+	public static int numVivas(int[][] board, int x, int y) {
+		int ret = 0;
+		if(board[x-1][y-1]==1)
+			ret++;
+		if(board[x+1][y+1]==1)
+			ret++;
+		if(board[x-1][y+1]==1)
+			ret++;
+		if(board[x+1][y-1]==1)
+			ret++;
+		if(board[x][y-1]==1)
+			ret++;
+		if(board[x-1][y]==1)
+			ret++;
+		if(board[x][y+1]==1)
+			ret++;
+		if(board[x+1][y]==1)
+			ret++;
 		
-		return retorno;
+		return ret;
 	}
 	
 	//Generates a number n of sets of random cells if a certain condition is met
-	public static void generaAleatorias(int[][] tablero, int n){
+	public static void generateRandom(int[][] board, int n){
 		if(Math.floor(Math.random()*(200-0+1)+0)%10==0) {
 			 for(int i=0;i<n;i++) {
 					int x = (int) Math.floor(Math.random()*(49-0+1)+0);
 					int y = (int) Math.floor(Math.random()*(199-0+1)+0);
-					tablero[x][y] = 1;
+					board[x][y] = 1;
 					
 					if(x-1 >= 0 && y-1>=0 && x+1<tamX && y+1<tamY) {
-						tablero[x][y] = 1;
-						tablero[x-1][y-1] = 1;
-						tablero[x][y+1] = 1;
-						tablero[x+1][y] = 1;
-						tablero[x-1][y] = 1;
+						board[x][y] = 1;
+						board[x-1][y-1] = 1;
+						board[x][y+1] = 1;
+						board[x+1][y] = 1;
+						board[x-1][y] = 1;
 					}	
 			}
 		 }
